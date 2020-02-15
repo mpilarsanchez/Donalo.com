7a6ff5b5af28f0db2d574f906689e487802a6f4c
mpilarsanchez <50201811+mpilarsanchez@users.noreply.github.com>
2020-02-15 20:17:35 -0300

    Add files via upload

---
+++ src/main/java/com/donalo/donalo/DonaloApplication.java
@@ -0,0 +1,13 @@
+package com.donalo.donalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonaloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonaloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donalo/donalo/DonaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donalo.donalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonaloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
