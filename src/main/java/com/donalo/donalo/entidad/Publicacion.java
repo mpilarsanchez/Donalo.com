d39f481bad6295bfa40939e817f66861ca93e32a
M Pilar Sanchez <sanchezmendozpilar@gmail.com>
2020-02-17 13:19:43 -0300

    nuevas entidades (usuario - mensaje y publicación)

---
+++ src/main/java/com/donalo/donalo/entidad/Publicacion.java
@@ -0,0 +1,41 @@
+package com.donalo.donalo.entidad;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.donalo.donalo.enumeracion.Seleccion;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+public class Publicacion {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+	
+	 @OneToOne
+	 private Foto foto;
+	 
+	 @ManyToOne
+	 private Usuario id_Usuario1;
+		
+	@ManyToOne
+	private Usuario id_Usuario2;
+		
+	private String descripcion;
+	
+    @Enumerated(EnumType.STRING)
+	private Seleccion seleccion;
+	 
+}
